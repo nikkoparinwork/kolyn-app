f28eff290d0953a40e76d1fa620eae8612240eed
nikkoparinwork <hello@nikkoparin.work>
2023-07-25 23:57:51 +0800

    Feat: Update main page, remove placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
 <script class="ts">
+    import Slides from '$lib/deck/slides.svelte'
      import "../app.css";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Nikko & Jennilyn</title>
+</svelte:head>
+
+<Slides />
